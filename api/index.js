91bcf5bfdcbd89417e6d4db3fb97ae72aea242d1
Lautaro Asat <lautaronorielasat@gmail.com>
2023-01-08 00:08:01 -0300

    Add news endpoints

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
 import leaderboard from '../db/leaderboard.json'
+import presidents from '../db/presidents.json'
+import teams from '../db/teams.json'
 
 const app = new Hono()
 
@@ -8,12 +10,37 @@ app.get('/', (ctx) => ctx.json([
   {
     endpoint: '/leaderboard',
     description: 'Returns the leaderboard'
+  },
+  {
+    endpoint: '/presidents',
+    description: 'Returns the presidents of kings league'
+  },
+  {
+    endpoint: '/teams',
+    description: 'Returns teams of kings league'
   }
 ])
 )
 
 app.get('/leaderboard', (ctx) => {
   return ctx.json(leaderboard)
+})
+
+app.get('/presidents', (ctx) => {
+  return ctx.json(presidents)
+})
+
+app.get('/presidents/:id', (ctx) => {
+  const id = ctx.req.param('id')
+  const foundPresident = presidents.find(president => president.id === id)
+
+  if (!foundPresident) return ctx.json({ message: 'President Dont found' }, 404)
+
+  return ctx.json(foundPresident)
+})
+
+app.get('/teams', (ctx) => {
+  return ctx.json(teams)
 })
 
 app.get('/static/*', serveStatic({ root: './' }))
