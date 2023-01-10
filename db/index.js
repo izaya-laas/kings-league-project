aa6b0310dcff76f94d4812c7468a6f6d03dcbbc8
Lautaro Asat <lautaronorielasat@gmail.com>
2023-01-10 13:29:57 -0300

    Create an auxiliar database file to read and write the file database

---
+++ db/index.js
@@ -0,0 +1,15 @@
+import { writeFile, readFile } from 'node:fs/promises'
+import path from 'node:path'
+
+const DB_PATH = path.join(process.cwd(), './db/')
+
+const readDBFile = async (fileName) => {
+  return await readFile(`${DB_PATH}/${fileName}.json`, 'utf-8').then(JSON.parse)
+}
+
+export const TEAMS = await readDBFile('teams')
+export const PRESIDENTS = await readDBFile('presidents')
+
+export const writeDBFile = (fileName, data) => {
+  return writeFile(`${DB_PATH}/${fileName}.json`, JSON.stringify(data, null, 2), 'utf-8')
+}
