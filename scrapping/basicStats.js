698b9ed519a7b85b29645f0c624e9905f6c65dbd
Lautaro Asat <lautaronorielasat@gmail.com>
2023-01-16 20:18:37 -0300

    Update basicStats

--- scrapping/basicStats.js
+++ scrapping/basicStats.js
@@ -1,28 +1,10 @@
 import { getStats } from './utils.js'
 import { writeDBFile } from '../db/index.js'
+import { logError, logInfo, logSuccess } from './log.js'
 
 const MVP_URL = 'https://kingsleague.pro/estadisticas/mvp/'
 const TOP_SCORER_URL = 'https://kingsleague.pro/estadisticas/goles/'
 const ASSISTS_URL = 'https://kingsleague.pro/estadisticas/asistencias/'
-
-const basicSelectors = {
-  ranking: {
-    selector: '.el-text-1',
-    type: 'number'
-  },
-  name: {
-    selector: '.el-text-3',
-    type: 'string'
-  },
-  player: {
-    selector: '.el-text-4',
-    type: 'string'
-  },
-  gamesPlayed: {
-    selector: '.el-text-5',
-    type: 'number'
-  }
-}
 
 const specialMVPSelector = {
   MVP: {
@@ -43,12 +25,26 @@ const specialAssistsSelector = {
   }
 }
 
-const MVP = await getStats(MVP_URL, { ...basicSelectors, ...specialMVPSelector })
+try {
+  logInfo('Scraping MVP List')
+  const MVP = await getStats(MVP_URL, specialMVPSelector)
+  logSuccess('MVP list scraped successfully')
 
-const TopScorer = await getStats(TOP_SCORER_URL, { ...basicSelectors, ...specialTopScorerSelector })
+  logInfo('Scraping Top Scorer List')
+  const TopScorer = await getStats(TOP_SCORER_URL, specialTopScorerSelector)
+  logSuccess('Top scorer list scraped successfully')
 
-const assists = await getStats(ASSISTS_URL, { ...basicSelectors, ...specialAssistsSelector })
+  logInfo('Scraping Top assists List')
+  const assists = await getStats(ASSISTS_URL, specialAssistsSelector)
+  logSuccess('Top assists list scraped successfully')
 
-writeDBFile('mvp', MVP)
-writeDBFile('topScorer', TopScorer)
-writeDBFile('assists', assists)
+  console.log('')
+
+  logInfo('Writing stats to database')
+  writeDBFile('mvp', MVP)
+  writeDBFile('topScorer', TopScorer)
+  writeDBFile('assists', assists)
+  logSuccess('Stats written successfully')
+} catch (e) {
+  logError(e)
+}
