99008afd4e868118df1da0c659f9c72682672ece
Lautaro Asat <lautaronorielasat@gmail.com>
2023-02-11 15:12:22 -0300

    Add button to top

---
+++ src/components/ScrollUp.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'preact/hooks'
+
+export default function ScrollUp() {
+  const [isActive, setIsActive] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 600) setIsActive(true)
+      else setIsActive(false)
+    })
+  }, [])
+
+  return (
+    <a
+      href='#top'
+      class={`p-2 fixed bottom-4 right-4 bg-[#fab60a] rounded-full border border-black transition-transform duration-700 ${
+        isActive ? 'translate-x-0' : 'translate-x-52'
+      }`}
+    >
+      <img src='/public/up.svg' class='w-4' />
+    </a>
+  )
+}
