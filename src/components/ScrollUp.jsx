2b4f99a6ed775a4ac94e720a0949ec4bc17e3347
Lautaro Asat <lautaronorielasat@gmail.com>
2023-02-13 23:36:12 -0300

    Change style on desktop

--- src/components/ScrollUp.jsx
+++ src/components/ScrollUp.jsx
@@ -13,11 +13,11 @@ export default function ScrollUp() {
   return (
     <a
       href='#top'
-      class={`p-2 fixed bottom-4 right-4 bg-[#fab60a] rounded-full border border-black transition-transform duration-700 ${
+      class={`p-2 sm:p-4 fixed z-20 bottom-4 right-4 bg-[#fab60a] rounded-full border border-black transition-transform duration-700 ${
         isActive ? 'translate-x-0' : 'translate-x-52'
       }`}
     >
-      <img src='/public/up.svg' class='w-4' />
+      <img src='/public/up.svg' class='w-4 sm:w-6' />
     </a>
   )
 }
