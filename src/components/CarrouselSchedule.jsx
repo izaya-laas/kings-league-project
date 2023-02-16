31fc9024f49b66383b70dd6609f5d650760e5428
Lautaro Asat <lautaronorielasat@gmail.com>
2023-02-16 00:45:58 -0300

    Add functionality to buttons next and previuos playdate

--- src/components/CarrouselSchedule.jsx
+++ src/components/CarrouselSchedule.jsx
@@ -5,10 +5,32 @@ import TableSchedule from './TableSchedule'
 export function CarrouselSchedule({ class: styles }) {
   const [schedule, setShedule] = useState([])
   const [currentPlayDate, setCurrentPlayDate] = useState({})
+  const [currentDay, setCurrentDay] = useState(0)
 
   const { playingDay, plays } = currentPlayDate
 
-  console.log(plays)
+  function updateNextDay() {
+    const limitNumberDay = schedule.length - 1
+
+    let number
+
+    if (currentDay === limitNumberDay) number = 0
+    else number = currentDay + 1
+
+    setCurrentDay(number)
+    setCurrentPlayDate(schedule[number])
+  }
+
+  function updatePreviousDay() {
+    const limitNumberDay = schedule.length - 1
+    let number
+
+    if (currentDay === 0) number = limitNumberDay
+    else number = currentDay - 1
+
+    setCurrentDay(number)
+    setCurrentPlayDate(schedule[number])
+  }
 
   useEffect(() => {
     async function extractSchedule() {
@@ -19,30 +41,28 @@ export function CarrouselSchedule({ class: styles }) {
       })
 
       setShedule(schedule)
+      setCurrentDay(currentDateIndex - 1)
       setCurrentPlayDate(schedule[currentDateIndex - 1])
     }
 
     extractSchedule()
   }, [])
 
-  currentPlayDate?.plays?.forEach((data) => {
-    console.log(data)
-  })
   return (
     <section className={` ${styles}`}>
       <header class='text-xs text-white uppercase bg-black font-bold p-3 rounded-t-xl'>
         <h2 className='text-center'>Kings League</h2>
       </header>
-      <aside className='flex items-center justify-between py-2 px-4'>
-        <button>
+      <aside className='flex items-center justify-between py-4 px-4'>
+        <button onClick={updatePreviousDay}>
           <img className='w-8' src='/public/arrow-left.svg' alt='icon left page' />
         </button>
         <h3 className='text-center'>{playingDay}</h3>
-        <button>
+        <button onClick={updateNextDay}>
           <img className='w-8' src='/public/arrow-right.svg' alt='icon right page' />
         </button>
       </aside>
-      <TableSchedule plays={currentPlayDate.plays} mdClass='2xl' />
+      <TableSchedule plays={plays} mdClass='2xl' />
     </section>
   )
 }
