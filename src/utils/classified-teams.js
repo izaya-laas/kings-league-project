49391eac8160a22f9f270ad2b8a0ac3f5a16fb82
Lautaro Asat <lautaronorielasat@gmail.com>
2023-02-18 00:47:35 -0300

    Export classified teams styles

---
+++ src/utils/classified-teams.js
@@ -0,0 +1,4 @@
+export const CLASSIFIED_STYLES =
+  'before:content-center before:w-2 before:h-4/5 before:top-1 before:left-1 before:block before:absolute before:bg-gradient-to-b before:from-[#2bc7ff] before:via-[#09f] before:to-[#2bc7ff]'
+
+export const MAX_CLASSIFIED_TEAMS = 8
